ed9f9bddc880e8b1e53cda20cbaaa13c3ac414e8
Yandira <y.condo16@email.com>
2023-11-06 18:39:18 -0500

    formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  // title = 'my-store';
+  
+  name = "Nicolas";
+  age = 18;
+  img = "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Irish_clover.jpg/180px-Irish_clover.jpg";
+  btnDisabled = true;
+  person = {
+    name: "Nicolas",
+    age: 18,
+    avatar: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0c/Irish_clover.jpg/180px-Irish_clover.jpg",
+  }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  increaseAge() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+  }
 }
+
